40613d5d7c36e3c52fc82c2eab7e1d0a9487a2a3
EvertonBorges <evertonsoaresborges@gmail.com>
2019-06-19 08:14:02 -0400

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/evertonborges/cursomc/CursomcApplication.java
+++ src/main/java/com/evertonborges/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.evertonborges.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.evertonborges.cursomc.domain.Cidade;
 import com.evertonborges.cursomc.domain.Cliente;
 import com.evertonborges.cursomc.domain.Endereco;
 import com.evertonborges.cursomc.domain.Estado;
+import com.evertonborges.cursomc.domain.Pagamento;
+import com.evertonborges.cursomc.domain.PagamentoComBoleto;
+import com.evertonborges.cursomc.domain.PagamentoComCartao;
+import com.evertonborges.cursomc.domain.Pedido;
 import com.evertonborges.cursomc.domain.Produto;
+import com.evertonborges.cursomc.domain.enums.EstadoPagamento;
 import com.evertonborges.cursomc.domain.enums.TipoCliente;
 import com.evertonborges.cursomc.repositories.CategoriaRepository;
 import com.evertonborges.cursomc.repositories.CidadeRepository;
 import com.evertonborges.cursomc.repositories.ClienteRepository;
 import com.evertonborges.cursomc.repositories.EnderecoRepository;
 import com.evertonborges.cursomc.repositories.EstadoRepository;
+import com.evertonborges.cursomc.repositories.PagamentoRepository;
+import com.evertonborges.cursomc.repositories.PedidoRepository;
 import com.evertonborges.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -83,6 +95,22 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(end1, end2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2019 10:32"), cli1, end1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2019 19:35"), cli1, end2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2019 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 
 }
---
+++ src/main/java/com/evertonborges/cursomc/domain/PagamentoComCartao.java
@@ -0,0 +1,29 @@
+package com.evertonborges.cursomc.domain;
+
+import javax.persistence.Entity;
+
+import com.evertonborges.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComCartao extends Pagamento {
+	private static final long serialVersionUID = 1L;
+
+	private Integer numeroParcelas;
+
+	public PagamentoComCartao() {
+	}
+
+	public PagamentoComCartao(Integer id, EstadoPagamento estadoPagamento, Pedido pedido, Integer numeroParcelas) {
+		super(id, estadoPagamento, pedido);
+		this.numeroParcelas = numeroParcelas;
+	}
+
+	public Integer getNumeroParcelas() {
+		return numeroParcelas;
+	}
+
+	public void setNumeroParcelas(Integer numeroParcelas) {
+		this.numeroParcelas = numeroParcelas;
+	}
+	
+}
