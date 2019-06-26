e895bee7fcb9b05678ed9a8f6ad694667ccfc2f7
EvertonBorges <evertonsoaresborges@gmail.com>
2019-06-26 09:05:08 -0400

    Validacao sintatica com Bean Validation

--- src/main/java/com/evertonborges/cursomc/services/CategoriaService.java
+++ src/main/java/com/evertonborges/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.evertonborges.cursomc.domain.Categoria;
+import com.evertonborges.cursomc.dto.CategoriaDTO;
 import com.evertonborges.cursomc.repositories.CategoriaRepository;
 import com.evertonborges.cursomc.services.exceptions.DataIntegrityException;
 import com.evertonborges.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,6 +54,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO categoriaDTO) {
+		return new Categoria(categoriaDTO.getId(), categoriaDTO.getNome());
 	}
 	
 }
