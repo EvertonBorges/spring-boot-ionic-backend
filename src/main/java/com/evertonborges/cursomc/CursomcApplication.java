b784a105d72d46d0e1fa158f8066d7bf442eff2f
EvertonBorges <evertonsoaresborges@gmail.com>
2019-07-28 17:13:16 -0400

    Criando profile de desenvolvimento

--- src/main/java/com/evertonborges/cursomc/CursomcApplication.java
+++ src/main/java/com/evertonborges/cursomc/CursomcApplication.java
@@ -1,25 +1,19 @@
 package com.evertonborges.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.evertonborges.cursomc.services.DBService;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	
-	@Autowired
-	private DBService dbService;
-
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		dbService.instantiateTestDatabase();
+
 	}
 
 }
