a566cbf3f25913d6fd6aa0df05a63b21a2f6fb3d
EvertonBorges <evertonsoaresborges@gmail.com>
2019-07-30 10:53:12 -0400

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/evertonborges/cursomc/config/DevConfig.java
+++ src/main/java/com/evertonborges/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.evertonborges.cursomc.services.DBService;
+import com.evertonborges.cursomc.services.EmailService;
+import com.evertonborges.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -30,4 +32,10 @@ public class DevConfig {
 		
 		return true;
 	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
+	
 }
--- src/main/java/com/evertonborges/cursomc/services/DBService.java
+++ src/main/java/com/evertonborges/cursomc/services/DBService.java
@@ -109,7 +109,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(cid1, cid2, cid3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "ebke@hotmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 		
 		Endereco end1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "39220834", cli1, cid1);
 		Endereco end2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, cid1);
